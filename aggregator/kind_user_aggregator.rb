c221b71b8eccc891d9aaf982dcf3e4c2f804b675
Nami <mando.lin925@gmail.com>
2021-09-19 20:19:23 +0900

    kind_user_aggregatorの変数名を修正

--- aggregator/kind_user_aggregator.rb
+++ aggregator/kind_user_aggregator.rb
@@ -10,33 +10,33 @@ class KindUserAggregator
   # 実装してください
   def exec
     #各チャンネルのハッシュから、keyが“messages”のvalueの配列を取ってくる。6つの要素を連結して、messageが並んでいる配列であるmessages_aryを作る
-    messages_ary = channel_names.map{|channel_name| load(channel_name)["messages"]}.flatten 
+    message_ary = channel_names.map{|channel_name| load(channel_name)["messages"]}.flatten 
 
     #各messageから、keyが“reactions”のvalueの配列を取ってくる。作成した配列を連結し、reactionが並んでいる配列であるreactions_aryを作る
-    reactions_ary = []
-    (0..messages_ary.length-1).each do |i|
-      reactions_ary.push(messages_ary[i]["reactions"])
+    reaction_ary = []
+    (0..message_ary.length-1).each do |i|
+      reaction_ary.push(message_ary[i]["reactions"])
     end
     
-    reactions_ary = reactions_ary.compact.flatten
+    reaction_ary = reaction_ary.compact.flatten
 
     #各reactionから、keyが“users”のvalueの配列を取ってくる。作成した配列を連結し、userが並んでいる配列であるusers_aryを作る
-    users_ary = []
-    (0..reactions_ary.length-1).each do |j|
-      users_ary.push(reactions_ary[j]["users"])
+    user_ary = []
+    (0..reaction_ary.length-1).each do |j|
+      user_ary.push(reaction_ary[j]["users"])
     end 
 
-    users_ary = users_ary.flatten
+    user_ary = user_ary.flatten
 
     #users_aryに並んでいるuser_idが、スタンプを押したuserのuser_idである（複数回スタンプを押した場合は、重複して含まれる）
     #users_aryにuser_idがそれぞれ何個ずつ記載されているかを数えて、個数が多い方から3人のユーザー情報を抽出する
-    users_count = []
-    users_ary.uniq.each do |l|
-      users_count.push({:user_id => l, :reaction_count => users_ary.count(l)})
+    user_count = []
+    user_ary.uniq.each do |l|
+      user_count.push({:user_id => l, :reaction_count => user_ary.count(l)})
     end
     
-    desc = users_count.sort_by! { |i| i[:reaction_count] }.reverse!
-    desc.first(3) 
+    desc = user_count.sort_by! { |i| i[:reaction_count] }.reverse!
+    desc.first(3)  
   end
 
   def load(channel_name)
