b3ae66732788823b7007f0d80ce948d441664323
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-03-03 01:15:08 +0900

    PopularMessageAggregatorの作成

--- aggregator/poupular_message_aggregator.rb
+++ aggregator/poupular_message_aggregator.rb
@@ -7,8 +7,18 @@ class PopularMessageAggregator
 
   # 実装してください
   def exec
-    # dummy
-    
+    messages = @channel_names.map{|channel_name| load(channel_name)["messages"] }.flatten
+    reaction_counts_by_id = []
+    messages.each do |message|
+      client_msg_id = message["client_msg_id"]
+      if client_msg_id
+        reaction_counts = message["reactions"].map{|reaction| reaction["count"] } if message["reactions"]
+        reaction_counts_by_id << ["#{client_msg_id}", reaction_counts.sum] if reaction_counts
+      end
+    end
+    target_message_info = reaction_counts_by_id.sort{|a, b| b[1] <=> a[1]}[0]
+    target_message = messages.find{|message| message["client_msg_id"] == target_message_info[0]}
+    {text: target_message["text"], reaction_count: target_message_info[1]}
   end
 
   def load(channel_name)
