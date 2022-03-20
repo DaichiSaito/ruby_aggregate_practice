6c80c61a4c422b6428f4baa49ae6e44428a63703
gyara658 <gyara658@gmail.com>
2022-03-20 14:02:29 +0900

    slack Data

--- aggregator/high_motivation_user_aggregator.rb
+++ aggregator/high_motivation_user_aggregator.rb
@@ -9,7 +9,14 @@ class HighMotivationUserAggregator
 
   # 実装してください
   def exec
-    
+    message_count = channel_names.map{
+      |channel_name| {
+        :channel_name => channel_name,
+        :message_count => load(channel_name)["messages"].length
+      }
+    }
+    desc = message_count.sort_by! { |a| a[:message_count] }.reverse!
+    desc.first(3)
   end
 
   def load(channel_name)
--- aggregator/poupular_message_aggregator.rb
+++ aggregator/poupular_message_aggregator.rb
@@ -8,7 +8,19 @@ class PopularMessageAggregator
   # 実装してください
   def exec
     # dummy
-    
+    message_ary = channel_names.map { |channel_name| load(channel_name)["messages"] }.flatten
+    # text_ary = message_ary.map{|message|{:text => message["text"],:reactions=> message["reactions"]}}.compact.flatten
+
+    count = Array.new(message_ary.length).map { |n| n = 0 }
+
+    message_ary.each_index do |i|
+      reactions = message_ary[i]["reactions"]
+      next if reactions.nil?
+      reactions.each_index do |j|
+        count[i] += reactions[j]["count"]
+      end
+    end
+    { :text => message_ary[count.index(count.max)]["text"], :reaction_count => count.max }
   end
 
   def load(channel_name)
