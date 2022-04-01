1ac8caa1416f4e5842ced1059e7e8601d3043025
nomos244 <nomos.geek@gmail.com>
2022-04-01 21:27:53 +0900

    Improve exec method in high_motivation_user_aggregator

--- aggregator/high_motivation_user_aggregator.rb
+++ aggregator/high_motivation_user_aggregator.rb
@@ -9,7 +9,12 @@ class HighMotivationUserAggregator
 
   # 実装してください
   def exec
-    
+    results = []
+    @channel_names.each do |name|
+      count = load(name)['messages'].count
+      results << {channel_name: name, message_count: count}
+    end
+    results.sort_by{|result| result[:message_count]}.reverse[0,3]
   end
 
   def load(channel_name)
